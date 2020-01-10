45cc11577d328ed897007eb977f8f163a4aca0a2
MilenaGiachetti <55102569+MilenaGiachetti@users.noreply.github.com>
2020-01-10 19:16:41 -0300

    Added search suggestion function

--- function/gif.js
+++ function/gif.js
@@ -10,29 +10,27 @@
 
 /*'https://api.giphy.com/v1/gifs/random?api_key='+apiKey+'&tag=adventure+time&fmt=html'*/
 
-
-
 const apiKey = 'HxeqWZObT2555n5inNEcjXprIyTed8Iq';
-var ctnBusqueda = document.getElementById('ctnBusqueda');
-var ctnTrending = document.getElementById('ctnTrending');
-var ctnSugerencias = document.getElementById('ctnSugerencias');
-var inputBusqueda = document.getElementById('inputBusqueda');
-var sugerenciasBusq = document.getElementById('sugerenciasBusq');
-var titleBusq = document.getElementById('titleBusq');
-var btnBusqueda = document.getElementById('btnBusqueda');
-var tagsSugeridos = document.getElementById('tagsSugeridos');
-var light = document.getElementById('light');
-var dark = document.getElementById('dark');
-var dropdownCaret = document.getElementById('dropdownCaret');
-var dropdownMenu = document.getElementById('dropdownMenu');
-var logo = document.getElementById('logo');
-var caret = document.getElementById('caret');
-var imgLupa = document.getElementById('imgLupa');
-var sugerencia1 = document.getElementById('resultado1');
-var sugerencia2 = document.getElementById('resultado2');
-var sugerencia3 = document.getElementById('resultado3');
+let ctnBusqueda = document.getElementById('ctnBusqueda');
+let ctnTrending = document.getElementById('ctnTrending');
+let ctnSugerencias = document.getElementById('ctnSugerencias');
+let inputBusqueda = document.getElementById('inputBusqueda');
+let sugerenciasBusq = document.getElementById('sugerenciasBusq');
+let titleBusq = document.getElementById('titleBusq');
+let btnBusqueda = document.getElementById('btnBusqueda');
+let tagsSugeridos = document.getElementById('tagsSugeridos');
+let light = document.getElementById('light');
+let dark = document.getElementById('dark');
+let dropdownCaret = document.getElementById('dropdownCaret');
+let dropdownMenu = document.getElementById('dropdownMenu');
+let logo = document.getElementById('logo');
+let caret = document.getElementById('caret');
+let imgLupa = document.getElementById('imgLupa');
 
-var Qsuggest = ['hola','morty','rick','alabama','dark','star wars','planet','love','planetario','planometria', 'Abadejo','Abadía','Abano','Abasto','Abdomen','Abecedario','Abedul','Abeja','Abejilla','Abertura','Abeto','Abismo','Ambiente','Abogada','Abogado','Abono','Abrazo','Abrelatas','Abrigo','Academia','Acceso','Accesorio','Acebiño','Acebo','Acedía','Aceite','Aceituna','Acelga','Acelgas','Acento','Acentor','plata','palta'];
+let suggestItems = ['Morty','Rick','Dark','Star Wars','Planet','Death Note', 'Love','Baby Yoda', 'Yoda','Cat', 'Juego', 'Dancing', 'Typing', 'Game','Weird', 'Pig','Funny','Animation','Movies','Anime','Unicorn','Werk', 'Adventure Time', 'Gravity Falls', 'SpongeBob', 'Avocado', 'Vaca', 'Disney', 'Joker','Japón', 'Corea', 'Strange', 'Ñoño', 'Hola', 'Area 51', 'Comida', 'Baby Shark', 'Quiero', 'Queso', '1984', 'Queen', 'X-Men', 'Zorro'];
+if(localStorage.getItem('suggestions') === '' || localStorage.getItem('suggestions') === null ){
+    localStorage.setItem("suggestions", JSON.stringify(suggestItems));
+}
 if(localStorage.getItem('theme') === 'theme-dark'){
     setTheme('theme-dark');
     logo.setAttribute('src',"assets/img/logodark.png");
@@ -47,7 +45,7 @@ function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
-dark.addEventListener('click',function(){
+dark.addEventListener('click',()=>{
     setTheme('theme-dark');
     logo.setAttribute('src',"assets/img/logodark.png");
     if(sugerenciasBusq.classList == 'hidden'){
@@ -58,7 +56,7 @@ dark.addEventListener('click',function(){
     cerrarMenu();
     event.stopPropagation();
 });
-light.addEventListener('click',function(){
+light.addEventListener('click',()=>{
     setTheme('theme-light');
     logo.setAttribute('src',"assets/img/logo.png");
     if(sugerenciasBusq.classList == 'hidden'){
@@ -75,13 +73,19 @@ function cerrarMenu (){
     dropdownMenu.classList.toggle('hidden');
     event.stopPropagation();
 }
-document.addEventListener('click', function(){
+document.addEventListener('click', ()=>{
     caret.classList.remove('open-caret');        
     dropdownMenu.classList.add('hidden');
 })
 
-//busqueda Qsuggest
-
+//busqueda 
+let sugerencia1 = document.getElementById('resultado1');
+let sugerencia2 = document.getElementById('resultado2');
+let sugerencia3 = document.getElementById('resultado3');
+let cuadroSug1 = document.getElementById('cuadroSug1');
+let cuadroSug2 = document.getElementById('cuadroSug2');
+let cuadroSug3 = document.getElementById('cuadroSug3');
+let noResults = false;
 function busqueda(){
     sugerenciasBusq.classList.add('hidden')
     ctnBusqueda.innerHTML = '';
@@ -97,16 +101,17 @@ function busqueda(){
     datos.then(function(respuesta){
         if(respuesta.data.length === 0){
             ctnBusqueda.innerHTML = "<p class='error'>OOPS! No se encontraron Gifs de ' "+inputBusqueda.value+"'.</p>";
+            noResults = true;
         }else{
             titleBusq.classList.remove('hidden');
             tagsSugeridos.classList.remove('hidden');
             titleBusq.textContent = "Resultados de: '" + inputBusqueda.value+"'";
             for (let i = 0; i < respuesta.data.length; i++) {
-                var ctnTotal = document.createElement('div');
+                let ctnTotal = document.createElement('div');
                 ctnTotal.setAttribute('class', 'ctnTotal');
-                var ctnImg = document.createElement('div');
+                let ctnImg = document.createElement('div');
                 ctnImg.setAttribute('class', 'ctnImg');
-                var img = document.createElement('div');
+                let img = document.createElement('div');
                 img.setAttribute('class', 'img');  
                 img.style.background = 'url('+respuesta.data[i].images.fixed_height_still.url+') center center';
                 /*background-size: auto 100%;*/
@@ -143,11 +148,28 @@ function busqueda(){
         }else{
             imgLupa.setAttribute('src', 'assets/img/lupainactive.svg')
         }
+        saveSug();
         inputBusqueda.value = '';
     });
 }
+function saveSug(){
+    let storageActual = JSON.parse(localStorage.suggestions);
+    let repeated = false;
+    let evaluatedText = inputBusqueda.value.toLowerCase();
+    for(let i = 0; i < storageActual.length; i++){
+        if(evaluatedText === storageActual[i].toLowerCase() || evaluatedText === ''){
+            repeated = true;
+            break;
+        }
+    }
+    if(repeated === false && noResults === false){
+        storageActual.push(evaluatedText.charAt(0).toUpperCase() + evaluatedText.substring(1));
+    }
+    localStorage.setItem("suggestions", JSON.stringify(storageActual));
+    noResults = false;
+}
 btnBusqueda.addEventListener('click', busqueda);
-inputBusqueda.addEventListener('keyup', function(){
+inputBusqueda.addEventListener('keyup', (event)=>{
     sugerenciasBusq.classList.remove('hidden');
     btnBusqueda.classList.add('btn');
     btnBusqueda.classList.add('btnBuscar');
@@ -157,42 +179,79 @@ inputBusqueda.addEventListener('keyup', function(){
     }else{
         imgLupa.setAttribute('src', 'assets/img/lupa.svg')
     }
-
     if (event.which === 13|| event.keyCode == 13) {
         busqueda();
-    }else if (event.which === 8|| event.keyCode == 8){
-        if(inputBusqueda.value === ''||inputBusqueda.value.length < 1){
-            sugerenciasBusq.classList.add('hidden');
-            btnBusqueda.classList.remove('btnBuscar');
-            btnBusqueda.classList.remove('btn');
-            btnBusqueda.classList.add('btnInactivo');
-            if(localStorage.getItem('theme') === 'theme-dark'){
-                imgLupa.setAttribute('src', 'assets/img/lupamiddlegray.svg')
-            }else{
-                imgLupa.setAttribute('src', 'assets/img/lupainactive.svg')
-            }
-
+    }else if ((event.which === 8|| event.keyCode == 8)
+            &&(inputBusqueda.value === ''
+            ||inputBusqueda.value.length < 1)){
+        sugerenciasBusq.classList.add('hidden');
+        btnBusqueda.classList.remove('btnBuscar');
+        btnBusqueda.classList.remove('btn');
+        btnBusqueda.classList.add('btnInactivo');
+        if(localStorage.getItem('theme') === 'theme-dark'){
+            imgLupa.setAttribute('src', 'assets/img/lupamiddlegray.svg')
+        }else{
+            imgLupa.setAttribute('src', 'assets/img/lupainactive.svg')
         }
-    }/*else{
-        Qsuggest.forEach(function(suggestion1){
-            if(suggestion1.toLowerCase().includes(inputBusqueda.value.toLowerCase()) && suggestion1 !== sugerencia2.textContent  && suggestion1 !== sugerencia3.textContent){
-                sugerencia1.textContent = suggestion1;
-                console.log(suggestion1);
+    }else{
+        cuadroSug1.classList.remove('hidden');
+        cuadroSug2.classList.remove('hidden');
+        cuadroSug3.classList.remove('hidden');
+        let searchSug = JSON.parse(localStorage.suggestions)
+        let end = 0;
+        let textSug = [];
+        for(let i = 0; i < searchSug.length; i++){
+            if(end === 3){
+                break;
+            }else if(searchSug[i].toLowerCase().startsWith(inputBusqueda.value.toLowerCase())){
+                textSug.push(searchSug[i]);
+                end++;
+            }      
+        }
+        if(end < 3){
+            for(let i = 0; i < searchSug.length; i++){
+                if(end === 3){
+                    break;
+                }else if(searchSug[i].toLowerCase().includes(inputBusqueda.value.toLowerCase()) 
+                        && (searchSug[i] !== textSug[0]) 
+                        && (searchSug[i] !== textSug[1])){
+                    textSug.push(searchSug[i]);
+                    end++;
+                }      
             }
-        });
-        Qsuggest.forEach(function(suggestion2){
-            if(suggestion2.toLowerCase().includes(inputBusqueda.value.toLowerCase()) && suggestion2 !== sugerencia1.textContent  && suggestion2 !== sugerencia3.textContent){
-                sugerencia2.textContent = suggestion2;
-                console.log(suggestion2);
-            }
-        });               
-        Qsuggest.forEach(function(suggestion3){
-            if(suggestion3.toLowerCase().includes(inputBusqueda.value.toLowerCase()) && suggestion3 !== sugerencia1.textContent && suggestion3 !== sugerencia2.textContent){
-                sugerencia3.textContent = suggestion3;
-                console.log(suggestion3);
-            }
-        });
-    }*/
+        }
+        if(textSug.length === 0){
+            cuadroSug3.classList.add('hidden');
+            cuadroSug2.classList.add('hidden');
+            cuadroSug1.classList.add('hidden');
+            sugerenciasBusq.classList.add('hidden');
+        } else if (textSug.length === 1){
+            sugerencia1.textContent = textSug[0].charAt(0).toUpperCase() + textSug[0].substring(1);
+            cuadroSug3.classList.add('hidden');
+            cuadroSug2.classList.add('hidden');
+        }else if (textSug.length === 2){
+            sugerencia1.textContent = textSug[0].charAt(0).toUpperCase() + textSug[0].substring(1);
+            sugerencia2.textContent = textSug[1].charAt(0).toUpperCase() + textSug[1].substring(1);
+            cuadroSug3.classList.add('hidden');
+        }else{
+            sugerencia1.textContent = textSug[0].charAt(0).toUpperCase() + textSug[0].substring(1);
+            sugerencia2.textContent = textSug[1].charAt(0).toUpperCase() + textSug[1].substring(1);
+            sugerencia3.textContent = textSug[2].charAt(0).toUpperCase() + textSug[2].substring(1);
+        }
+    }
+})
+//evento click sugerencias
+sugerencia1.addEventListener('click', ()=>{
+    inputBusqueda.value = sugerencia1.textContent;
+    busqueda();
+})
+sugerencia2.addEventListener('click', ()=>{
+    inputBusqueda.value = sugerencia2.textContent;
+    busqueda();
+})
+sugerencia3.addEventListener('click', ()=>{
+    inputBusqueda.value = sugerencia3.textContent;
+    busqueda();
 })
 //funcion cerrar sugerencias
 function close (){
@@ -208,17 +267,17 @@ async function giphySugerencias(sugerencia){
 }
 function sugerir(sugerencia){
     sugerenciasDatos = giphySugerencias(sugerencia);
-    sugerenciasDatos.then(function(respuesta){
-        var ctnTotal = document.createElement('div');
+    sugerenciasDatos.then((respuesta)=>{
+        let ctnTotal = document.createElement('div');
         ctnTotal.setAttribute('class', 'ctnTotal');
-        var title = document.createElement('div');
+        let title = document.createElement('div');
         title.setAttribute('class', 'title');
-        var content = document.createElement('p');
+        let content = document.createElement('p');
         content.textContent = '#'+sugerencia/*respuesta.data.title*/;
-        var close = document.createElement('img');
+        let close = document.createElement('img');
         close.setAttribute('src', 'assets/img/close.svg');
         //funcion de cerrar
-        close.addEventListener('click',function(){
+        close.addEventListener('click',()=>{
             ctnTotal.remove();
             let index = Math.floor(Math.random() * (tagsSugerir.length - 0)) + 0;
             sugerir(tagsSugerir[index]);
@@ -226,14 +285,14 @@ function sugerir(sugerencia){
         title.appendChild(content);
         title.appendChild(close);
         ctnTotal.appendChild(title);
-        var ctnImg = document.createElement('div');
+        let ctnImg = document.createElement('div');
         ctnImg.setAttribute('class', 'ctnImg');
-        var img = document.createElement('div');
+        let img = document.createElement('div');
         img.setAttribute('class', 'img');
         img.style.background = 'url('+respuesta.data.images.fixed_height/*_still*/.url+') center center';
             /*background-size: auto 100%;*/
         img.style.backgroundSize = 'cover';
-        var btn = document.createElement('button');
+        let btn = document.createElement('button');
         btn.setAttribute('class', 'btnSecundario');
         btn.innerHTML = "<a href='"+respuesta.data.bitly_url+"' target='_blank'> Ver más...</a>";
         img.appendChild(btn);
@@ -255,13 +314,13 @@ async function giphyTrending(){
 }
 trendingDatos = giphyTrending();
 
-trendingDatos.then(function(respuesta){
+trendingDatos.then((respuesta)=>{
     for (let i = 0; i < respuesta.data.length; i++) {
-        var ctnTotal = document.createElement('div');
+        let ctnTotal = document.createElement('div');
         ctnTotal.setAttribute('class', 'ctnTotal');
-        var ctnImg = document.createElement('div');
+        let ctnImg = document.createElement('div');
         ctnImg.setAttribute('class', 'ctnImg');
-        var img = document.createElement('div');
+        let img = document.createElement('div');
         img.setAttribute('class', 'img');
         img.style.background = 'url('+respuesta.data[i].images.fixed_height_still.url+') center center';
         img.style.backgroundSize = 'auto 100%';
@@ -278,33 +337,22 @@ trendingDatos.then(function(respuesta){
         img.addEventListener('mouseleave', function(){
             this.style.background = 'url('+respuesta.data[i].images.fixed_height_still.url+') center center'
             this.style.backgroundSize = 'auto 100%';
-
         });
         //url al clickear
-        img.addEventListener('click', function(){
+        img.addEventListener('click', ()=>{
             window.open(respuesta.data[i].url,'_blank');
         });
-        var content = document.createElement('p');
+        let content = document.createElement('p');
         content.setAttribute('class', 'title');
-
-        //str = respuesta.data[i].title.substring(str.indexOf("G") + 1); 
         let titleStr = respuesta.data[i].title.charAt(0).toUpperCase() + respuesta.data[i].title.substring(1); 
         content.textContent = titleStr.slice(0, titleStr.indexOf("GIF")); 
         if(content.textContent === ''){ 
-            content.textContent = 'No title'; 
+            content.classList.add('hidden'); 
         }
-        /*if(respuesta.data[i].images.fixed_height_still.width >= 345){
-            div.classList.add('gifRectangulo');
-        }else{
-            div.classList.add('gifCuadrado');
-        }*/
         img.appendChild(content);
         ctnImg.appendChild(img);
         ctnTotal.appendChild(ctnImg);
         ctnTrending.appendChild(ctnTotal);
-        /*ctnTotal.addEventListener('mouseenter', function(){
-            ctnTotal.classList.add('hidden');
-        })*/
     }
 });
 
